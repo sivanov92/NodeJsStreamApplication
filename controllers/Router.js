0aea8a51d5ea034669ec985ec5ba23cf0a07623c
Stanislav.Ivanov <stanislav_ivanov9213@abv.bg>
2021-03-21 22:35:56 +0200

    Auth completed

--- controllers/Router.js
+++ controllers/Router.js
@@ -1,11 +1,21 @@
 var express = require('express')
 var app = express()
 var router = express.Router();
+var jwt = require('jsonwebtoken');
+var config = require('./config.js');
 
 var userRouter = require('UserController.js');
 
 router.use(function(req,res,next){
-  //Set up OAUTH or other authentication for API services for the FE
+  let token = req.get('Authorization');
+  jwt.verify(token, config.API_SECRET_KEY, function(err, decoded) {
+    if(err){
+      res.status(401).send('Unauthorized to send request !');
+    }
+  });
+  next();
 });
 
 router.use('/',userRouter);
+
+module.exports = router;
