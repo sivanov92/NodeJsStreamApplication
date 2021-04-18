e2e01c90a28ad464f7bc063943baa23b14f23325
Stanislav Ivanov <stanislav_ivanov9213@abv.bg>
2021-04-18 22:17:51 +0300

    env vars

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 var routing = require('./controllers/Router.js');
 process.env.DATABASE_URL = 'mysql://doadmin:t52h3nnoqtlj3bfu@db-mysql-fra1-95766-do-user-8480655-0.b.db.ondigitalocean.com:25060/defaultdb?ssl-mode=REQUIRED';
 
+var config = require('./config');
+process.env.HOSTNAME = config.host;
+process.env.PORT = config.db_port;
+process.env.USERNAME = config.username;
+process.env.PASSWORD = config.password;
+process.env.DATABASE = config.dbname;
+
 var express = require('express');
 var router = express.Router();
 
