c60c4a90ce4e5e17856f956002e05f64c2d31f39
Stanislav Ivanov <stanislav_ivanov9213@abv.bg>
2021-04-24 17:25:11 +0300

    a1

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 var routing = require('./controllers/Router.js');
 var express = require('express');
 var router = express.Router();
+var app = express();
 
+const PORT = process.env.PORT || 5000;
 
 router.use('/',routing);
 
+module.exports = router;
+
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
