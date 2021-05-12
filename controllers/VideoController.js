506c5f3ac36cab7f1e614cd311956385ef8efecf
Stanislav Ivanov <stanislav_ivanov9213@abv.bg>
2021-05-12 22:57:45 +0300

    Video Api updates

--- controllers/VideoController.js
+++ controllers/VideoController.js
@@ -1,3 +1,4 @@
+const formData = require("form-data");
 var express = require('express');
 var config = require('../config.js');
 const Video = require('../models/Video.js');
@@ -6,6 +7,7 @@ const fileupload = require('express-fileupload');
 const fetch = require('node-fetch');
 var app = express();
 const fs = require('fs/promises');
+const fsys = require('fs');
 var router = express.Router();
 
 router.use(express.json()) // for parsing application/json
@@ -35,8 +37,9 @@ router.get('/author/:authorID',async (req,res)=>{
         res.status(200).json(JSON.stringify(videos));
         return;
     }
-    const author = User.findOne({where : {id:authorID}});
-     res.json(JSON.stringify(author.getVideos()));
+    const author = await User.findOne({where : {id:authorID}}).catch((e)=>{console.log(e);});
+    const videos = await author.getVideos().catch(e=>{console.log(e);});
+     res.json(JSON.stringify(videos));
     });
    
 //Post a new video
@@ -44,25 +47,26 @@ router.post('/',async (req,res)=>{
     let body = req.body;
     if(!req.files.file){
         return res.status(400).send('No files named /"file/" were uploaded.');
-        return;
     }
     let video_temp_name = 'tmp/'+req.files.file.name;
 
     let write_data = await fs.appendFile(video_temp_name,req.files.file.data,{flag:'wx'}).catch((e) => {console.log(e);});
 
-    let file_loc = {'file':req.files.file};
-
-    let args = {
+    let form = new formData();
+    const buffer = fsys.readFileSync(video_temp_name);
+    form.append('file', buffer, {
+        filename: video_temp_name
+    });   
+     let args = {
         'method':"POST",
-        'body':JSON.stringify(file_loc),
+        'body': form,
         'headers':{
-            "content-type":"multipart/form-data",
             "X-Auth-Email":config.cloudflare_email,
-            "X-Auth-Key":config.cloudflare_edit_stream_token
+            "X-Auth-Key":config.cloudflare_stream_key
         }
     };   
     var videos = await fetch(base_cloudflare_endpoint,args).catch((e) => {console.log(e);});
-    console.log(videos);
+    const data = await videos.json();
 
     let delete_data = await fs.unlink(video_temp_name);
       
@@ -70,19 +74,21 @@ router.post('/',async (req,res)=>{
         console.log('success');
         let video_body = {
             'title':body.title,
-            'author':body.author,
-            'file':videos.playback.hls,
-            'uid':videos.uid,
-            'thumbnail':videos.thumbnail
+            'UserId':body.author,
+            'file':data.result.playback.hls,
+            'uid':data.result.uid,
+            'thumbnail':data.result.thumbnail
         };
         for(key in video_body){
-            if(video_body.key == null || video_body.key==''){
+            if(video_body[key] == null || video_body[key]==''){
                 res.status(403).send(`Please fill all fields , ${key} is missing`);
                 return;
             }
            }    
-         var video_author =   await User.findOne({where : {email : body.author}}).catch((e) => {console.log(e);});
-         const new_video =  await Video.create(video_body).catch((e) => {console.log(e);});
+         var video_author =   await User.findOne({where : {id : body.author}}).catch((e) => {console.log(e);});
+
+         var new_video =  await Video.create(video_body).catch((e) => {console.log(e);});
+
          await video_author.addVideo(new_video).catch((e) => {console.log(e);});
          res.status(201).json(JSON.stringify(new_video));
         return;
@@ -95,12 +101,8 @@ router.post('/',async (req,res)=>{
 router.put('/:uid',async(req,res)=>{
  let uid_param = req.params.uid;
  let title_param = req.body.title;
- const vid = await Video.update({title:title_param},{uid:uid_param});
- if(vid != null ){
-     res.status(200);
-     return;
- }
- res.status(400);
+ const vid = await Video.update({title:title_param},{uid:uid_param}).catch((e)=>{console.log(e);});
+ res.status(vid.status);
 });
 
 //Delete a video
@@ -109,18 +111,21 @@ router.delete('/:uid',async(req,res)=>{
  let args = {
     'method':"DELETE",
     'headers':{
-        "content-type":"application/json",
         "X-Auth-Email":config.cloudflare_email,
-        "X-Auth-Key":config.cloudflare_edit_stream_token
+        "X-Auth-Key":config.cloudflare_stream_key
     }
 };   
-var videos_del = await fetch(`${base_cloudflare_endpoint}/${uid}`,args);
+var videos_del = await fetch(`${base_cloudflare_endpoint}/${uid_param}`,args).catch((e)=>{console.log(e);});
+var res = await videos_del.json().catch((e)=>{console.log(e);});
+
 if(videos_del.status == 200){
-    await Video.destroy({ where : {uid : uid_param}});
+    await Video.destroy({ where : {uid : uid_param}}).catch((e)=>{console.log(e);});
     res.status(200);
+    res.end();
     return;
 }
-res.status(400);
+res.status(videos_del.status);
+res.end();
 });
 
 module.exports = router;
