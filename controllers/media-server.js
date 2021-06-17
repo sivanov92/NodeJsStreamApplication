3ded5be4feb3a9ad4d00d4e61d5150bae8c31c1e
Stanislav Ivanov <stanislav_ivanov9213@abv.bg>
2021-06-17 23:28:08 +0300

    nms update

--- controllers/media-server.js
+++ controllers/media-server.js
@@ -12,18 +12,6 @@ const config = {
       port: 8000,
       mediaroot: './media',
       allow_origin: '*'
-    },
-    trans: {
-      ffmpeg: "ffmpeg", // "/usr/local/bin/ffmpeg",// "C:\\ffmpeg\\bin\\ffmpeg.exe",
-      tasks: [
-        {
-          app: 'live',
-          hls: true,
-          hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
-          dash: true,
-          dashFlags: '[f=dash:window_size=3:extra_window_size=5]'
-        }
-      ]
     }
   };
  
