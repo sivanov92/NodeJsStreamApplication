4d2936a240a8068321255333119004453f923b76
Stanislav Ivanov <stanislav_ivanov9213@abv.bg>
2021-06-26 19:26:24 +0300

    update

---
+++ config.js
@@ -0,0 +1,28 @@
+
+let dbname = 'defaultdb';
+let host = 'db-mysql-fra1-95766-do-user-8480655-0.b.db.ondigitalocean.com';
+let username = 'doadmin';
+let password = 't52h3nnoqtlj3bfu';
+let db_port = '25060';
+
+let API_SECRET_KEY = 'Qp3uDfFH7nfTOCiKeqAS';
+
+let CloudflareEditStreamToken = 'DVd0YQCwT8Fxb-VPJZ5X0JXJoJd3Inwu923U26tk';
+let CloudflareReadStreamToken = 'wfz51HaTx4oPIMsv3ApQAT0XorBkTJu7YyCHgx7k';
+let CloudflareEmail = 'stanislav_ivanov9213@abv.bg';
+let CloudflareAccID = '9782d933505b9ed9cfd4cbad528ef454';
+
+module.exports = {
+    'dbname' : dbname,
+    'host'   : host,
+    'username':username,
+    'password':password,
+    'db_port':db_port,
+    'API_SECRET_KEY':API_SECRET_KEY,
+    'cloudflare_edit_stream_token':CloudflareEditStreamToken,
+    'cloudflare_read_stream_token':CloudflareReadStreamToken,
+    'cloudflare_email':CloudflareEmail,
+    'cloudflare_acc_id':CloudflareAccID,
+    'cloudflare_stream_key':'03d293b516dc9accf2f93d8a933f2868ad7dc'
+};
+
